05ddf254b110448b9001876e9825b2920ab29cf2
Jason Albino <jason.albino@gruposolutio.com>
2022-10-01 14:31:17 +0200

    get users from collection fireStore +toas

--- lib/Home/Home.dart
+++ lib/Home/Home.dart
@@ -1,9 +1,56 @@
 
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
-class Home extends StatelessWidget{
+class Home extends StatefulWidget{
+
+
+
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  FirebaseFirestore db=FirebaseFirestore.instance;
+   String nombre="bien venido a la app";
+
+  void getProfile() async {
+
+    final docs=db.collection("/Profiles").doc(FirebaseAuth.instance.currentUser?.uid);
+
+    await docs.get().then((DocumentSnapshot doc) {
+      final data=doc.data() as Map<String,dynamic>;
+      Fluttertoast.showToast(
+          msg: "This is a Toast message",  // message
+          toastLength: Toast.LENGTH_SHORT, // length
+          gravity: ToastGravity.CENTER,    // location
+          timeInSecForIosWeb: 4              // duration
+      );
+      setState(() {
+        nombre=data?['name'];
+      });
+      onError: (e) => print("Error getting document: $e");
+    }
+
+    );
+
+
+
+  }
+  @override
+  void initState() {
+
+    super.initState();
+    getProfile();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -15,7 +62,7 @@ class Home extends StatelessWidget{
 
         child: Column(
           children: [
-            Text("Homeeee"),
+            Text(nombre),
             OutlinedButton(
               style: OutlinedButton.styleFrom(
                 backgroundColor: Colors.purple,
